282b53749f588961b2df35828fa8e42e261c0dea
NhatHONGUYEN <hnnhat67@gmail.com>
2024-04-29 16:27:09 +0200

    App.jsx > Add my name

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import reactSwc from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    watch: {
-      usePolling: true,
-    },
-  },
-  plugins: [react()],
+  plugins: [reactSwc()],
 });
