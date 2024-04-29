757efbc131580ea990cf80ea5dd2b04908acc9a1
NhatHONGUYEN <hnnhat67@gmail.com>
2024-04-29 17:01:59 +0200

    🚧 F01 : APP > Add Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div>Salut Nhat</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/About" element={<About />} />
+      <Route path="/Projects" element={<Projects />} />
+      <Route path="/Contact" element={<Contact />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  );
 }
 
 export default App;
